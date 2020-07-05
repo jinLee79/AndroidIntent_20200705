94c0da3945d47ab9ba80032ec5ef63a25a90bac9
jslee <etoileettoi@gmail.com>
2020-07-05 14:43:46 +0900

    전화 걸기(DIAL) 화면만 그림

--- app/src/main/java/kr/co/tjoeun/androidintent_20200705/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/androidintent_20200705/MainActivity.kt
@@ -2,10 +2,17 @@ package kr.co.tjoeun.androidintent_20200705
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        dial버튼 이벤트
+        dialBtn.setOnClickListener {
+
+
+        }
     }
 }
