57420771af469cd9d800a2bb16650f20eb826e7a
jslee <etoileettoi@gmail.com>
2020-07-05 15:36:10 +0900

    네이버 링크 보내기 기능

--- app/src/main/java/kr/co/tjoeun/androidintent_20200705/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/androidintent_20200705/MainActivity.kt
@@ -48,9 +48,13 @@ class MainActivity : AppCompatActivity() {
             myIntent.putExtra("sms_body", content)
 
             startActivity(myIntent)
+        }
 
-
-
+        naverLinkBtn.setOnClickListener {
+//            Uri에 실제 인터넷 주소 대입
+            val myUri = Uri.parse("https://naver.com")
+            val myIntent = Intent(Intent.ACTION_VIEW, myUri)
+            startActivity(myIntent)
         }
     }
 }
